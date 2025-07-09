64e2e4ba83795d25ec7ee0dcdaa8b9189e7760b1
nimilgp <nimilgeorgepradeepngp@gmail.com>
2025-07-09 23:44:28 +0530

    break text content into chunks

---
+++ steps/2.chunker.py
@@ -0,0 +1,19 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+
+pdf_path = "./alice-in-wonderland.pdf"
+loader = PyPDFLoader(pdf_path)
+document = loader.load()
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,  
+    chunk_overlap=200 
+)
+chunks = [] 
+for page in document:
+    pageChunks = text_splitter.split_text(page.page_content)
+    print("\n\n\n")
+    print("#######################################################################")
+    for chunk in pageChunks:
+        print(chunk)   
+
