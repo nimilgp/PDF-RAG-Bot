c5af10555eb272b9f14e753d185659f4d3125db3
nimilgp <nimilgeorgepradeepngp@gmail.com>
2025-07-25 23:16:06 +0530

    make llm querring function stream to caller

--- rag_helper.py
+++ rag_helper.py
@@ -57,13 +57,13 @@ def add_pdf_to_vector_store(pdf_path, vector_store, original_filename):
         except Exception as e:
             print(f"Failed to process {pdf_path}. Error: {e}")
 
-def querry_the_llm(q,vector_store):
+def querry_the_llm(q, vector_store):
     print(f"\nQuerying for: '{q}'")
     context = vector_store.similarity_search(query=q, k=10)
     print()
     print(context)
     system_message_content = "You are a helpful AI assistant. Use the following information to answer the user's question. If the answer is not present in the provided information, state that you don't have enough information."
-    
+
     if not context:
         system_message_content = "No Relevent information found from the pdf knowledge set"
     else:
@@ -81,9 +81,8 @@ def querry_the_llm(q,vector_store):
             stream=True,
         )
 
-        print("\nResponse:")
         for chunk in stream:
-            print(chunk['message']['content'], end='', flush=True)
-        print()
+            yield chunk['message']['content']
+
     except Exception as e:
-        print(f"An error occurred while querying the model: {e}")  
+        print(f"An error occurred while querying the model: {e}")
